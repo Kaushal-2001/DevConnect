eb9de2989abfaf5c145c3fc3d0ac84b8083ea657
Kaushal Apastambh <apastambhkaushal2001@gmail.com>
2026-06-15 12:52:22 -0700

    Add login API

--- src/app.js
+++ src/app.js
@@ -25,6 +25,27 @@ app.post("/signup", async (req, res) => {
     }
 })
 
+app.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await User.findOne({ email: email })
+        if (!user) {
+            throw new Error("Invalid credentials")
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if (isPasswordValid) {
+            res.send("login succesfull")
+        }
+        else {
+            throw new Error("Invalid credentials")
+        }
+    }
+    catch(err) {
+            res.status(400).send("Err: " + err.message)
+        }
+    
+    })
+
 app.get("/user", async (req, res) => {
     const userEmail = req.body.email
     try {
